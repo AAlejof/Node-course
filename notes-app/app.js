47c0a0e3c057b279fb63aec966ed1675ed1e4fb5
Alejo Fariña <scaramouch11@gmail.com>
2024-01-03 15:04:09 -0300

    section 3 - 8 & 9

---
+++ notes-app/app.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+
+// fs.writeFileSync('notes.txt', 'This file was created by Node.js. My name is Alejo.')
+
+fs.appendFileSync('notes.txt', 'I want to append this try this function')
