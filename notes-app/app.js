008308246ca9e41130d67363076dcd3d71578d23
Alejo Fariña <scaramouch11@gmail.com>
2024-01-05 16:51:06 -0300

    Section 4 - 17

--- notes-app/app.js
+++ notes-app/app.js
@@ -6,17 +6,28 @@ const getNotes = require('./notes.js');
 yargs.version('1.1.0')
 
 // Create add command
-
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function() {
-        console.log('Adding a new note!');
+    builder: {
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: '+ argv.title);
+        console.log('Body: '+ argv.body);
     }
 })
 
 // Create remove command
-
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
@@ -26,7 +37,6 @@ yargs.command({
 })
 
 // Create list command
-
 yargs.command({
     command: 'list',
     describe: 'List all notes',
@@ -36,7 +46,6 @@ yargs.command({
 })
 
 // Create read command
-
 yargs.command({
     command: 'read',
     describe: 'Read a note',
@@ -45,6 +54,4 @@ yargs.command({
     }
 })
 
-// add, removem read, list
-
-console.log(yargs.argv);
+yargs.parse()
