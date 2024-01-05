b18743136cb965ac188b1e62fb8653b3c488f8c8
Alejo Fariña <scaramouch11@gmail.com>
2024-01-05 18:58:26 -0300

    Section 4 - 20

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,72 @@
+const fs = require('fs');
+
 const getNotes = function () {
     return 'Your notes...';
+};
+
+const addNote = function (title, body) {
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        }
+        )
+        savedNotes(notes)
+        console.log('New note added!');
+    } else {
+        console.log('Note title taken!');
+    }
+
+
+    savedNotes(notes);
+};
+
+const savedNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
 }
 
-module.exports = getNotes;
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+
+    }    
+};
+
+
+const removeNote = function (title) {
+    const notes = loadNotes();
+    console.log(title);
+
+    const searchedNote = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    if (searchedNote.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        }
+        )
+        savedNotes(notes)
+        console.log('New note added!');
+    } else {
+        console.log('Note title taken!');
+    }
+}
+
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+};
