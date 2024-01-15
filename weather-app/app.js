ef36e4956942cca1d4064d5443801136a26d1a7a
Alejo Fariña <scaramouch11@gmail.com>
2024-01-15 16:29:37 -0300

    Section 6 - 32

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,8 @@ const request = require('postman-request');
 
 const url = 'http://api.weatherstack.com/current?access_key=02add3870472c3d1731ec3a729289849&query=37.8267,-122.4233'
 
-request({ url: url}, ( error, response ) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({ url: url, json: true}, ( error, response ) => {
+    const data = response.body.current;
+
+    console.log(`It is currently ${data.temperature}ºC out and it feels like ${data.temperature}ºC`);
 })
