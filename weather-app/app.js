e29dde79d14b948456cb6b94dfd14e889a50c3f4
Alejo Fariña <scaramouch11@gmail.com>
2024-01-16 15:11:39 -0300

    Section 6 - 34

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,15 +3,35 @@ const request = require('postman-request');
 const url = 'http://api.weatherstack.com/current?access_key=02add3870472c3d1731ec3a729289849&query=37.8267,-122.4233&units=m'
 
 request({ url: url, json: true}, ( error, response ) => {
-    const data = response.body.current;
+    if (error) {
 
-    console.log(`${data.weather_descriptions[0]}. It is currently ${data.temperature}ºC out and it feels like ${data.feelslike}ºC`);
+        console.log('Unable to connect to weather service!');
+
+    } else if (response.body.error) {
+
+        console.log('Unable to find location!');
+    
+    } else {
+        const data = response.body.current;
+    
+        console.log(`${data.weather_descriptions[0]}. It is currently ${data.temperature}ºC out and it feels like ${data.feelslike}ºC`);
+    }
 })
 
 const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWFsZWpvZiIsImEiOiJjbHJnaHp6M3UwaGtpMnJwYmgxc3hvMGQ2In0.QHwURSlBQFlrjEh571Zh6g&limit=1'
 
 request({ url: geoUrl, json: true}, ( error, response ) => {
-    const data = response.body.features;
+    if (error) {
+       
+        console.log('Unable to connect to geolocation!');
 
-    console.log(`The latitude is ${data[0].center[1]}º and the longitude is ${data[0].center[0]}º`);
+    } else if (response.body.features.length === 0) {
+
+        console.log('Unable to find location! Try another search...');
+
+    } else {
+        const data = response.body.features;
+    
+        console.log(`The latitude is ${data[0].center[1]}º and the longitude is ${data[0].center[0]}º`);
+    }
 })
