969d53a9762c772c71b80d532d776afa6bc12a4a
Alejo Fariña <scaramouch11@gmail.com>
2024-01-16 13:09:53 -0300

    Section 6 - 33

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,17 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=02add3870472c3d1731ec3a729289849&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=02add3870472c3d1731ec3a729289849&query=37.8267,-122.4233&units=m'
 
 request({ url: url, json: true}, ( error, response ) => {
     const data = response.body.current;
 
-    console.log(`It is currently ${data.temperature}ºC out and it feels like ${data.temperature}ºC`);
+    console.log(`${data.weather_descriptions[0]}. It is currently ${data.temperature}ºC out and it feels like ${data.feelslike}ºC`);
+})
+
+const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWFsZWpvZiIsImEiOiJjbHJnaHp6M3UwaGtpMnJwYmgxc3hvMGQ2In0.QHwURSlBQFlrjEh571Zh6g&limit=1'
+
+request({ url: geoUrl, json: true}, ( error, response ) => {
+    const data = response.body.features;
+
+    console.log(`The latitude is ${data[0].center[1]}º and the longitude is ${data[0].center[0]}º`);
 })
