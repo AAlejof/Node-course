55b6f24d918e198ec666e8a07120a60216ee4934
Alejo Fariña <scaramouch11@gmail.com>
2024-01-22 16:34:16 -0300

    Section 8 - 57

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,19 @@
+
 console.log('Holiiii');
+
+fetch('https://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+    })
+})
+
+fetch('http://localhost:3000/weather?address=boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(error);
+        } else {
+            console.log(data.location);
+            console.log(data.forecast);
+        }
+    })
+})
