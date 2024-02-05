890ab5a80d4489c42efa3d86372ba1c1f1758a8f
Alejo Fariña <scaramouch11@gmail.com>
2024-02-05 15:26:01 -0300

    Section 10 - 80

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -187,51 +187,113 @@ const client = new MongoClient(uri);
 //   run().catch(console.dir);
 
 //---------Find-------
+// async function run() {
+//     try {
+
+//         // Get the database and collection on which to run the operation
+//         const database = client.db("task-manager");
+//         const users = database.collection("users");
+
+//         const user = await users.find({}).toArray((error, users) => {
+//         });
+
+//         const count = await users.countDocuments({});
+//         console.log(user);
+//         console.log(count);
+
+//     } finally {
+//         await client.close();
+//     }
+// }
+// run().catch(console.dir);
+
+
+// //-----FindOne by Id and Find not completed tasks--------
+// async function run() {
+//     try {
+//         // Get the database and collection on which to run the operation
+//         const database = client.db("task-manager");
+//         const tasks = database.collection("tasks");
+
+//         // Find the last document inserted by sorting based on the _id field in descending order
+//         const lastInsertedDocument = await tasks.findOne({}, { sort: { _id: -1 } });
+
+//         // Find not completed tasks
+//         const notCompleted = await tasks.find({ completed: false }).toArray();
+
+//         if (lastInsertedDocument && notCompleted) {
+//             console.log("Last Inserted Document:", lastInsertedDocument);
+//             console.log("The not completed tasks are:", notCompleted[0].description);
+//         } else {
+//             console.log("No documents found.");
+//         }
+
+//     } finally {
+//         await client.close();
+//     }
+// }
+
+// run().catch(console.dir);
+
+// //------------updateOne users---------
+// async function run() {
+//     try {
+//         const database = client.db("task-manager");
+//         const users = database.collection("users");
+//         // Create a filter for users with the title "Alejo"
+//         const filter = { name: "Facu" };
+//         /* Set the upsert option to insert a document if no documents match
+//         the filter */
+//         const options = { upsert: true };
+//         // Specify the update to set a value for the age field
+//         const updateDoc = {
+//             $set: {
+//                 name: "Facu",
+//                 age: 36,
+//                 potato: false
+//             },
+//         };
+//         // Update the first document that matches the filter
+//         const result = await users.updateOne(filter, updateDoc, options);
+
+//         // Print the number of matching and modified documents
+//         console.log(
+//             `Users: ${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+//         );
+//     } finally {
+//         // Close the connection after the operation completes
+//         await client.close();
+//     }
+// }
+// // Run the program and print any thrown errors
+// run().catch(console.dir);
+
+//------------updateMany tasks---------
 async function run() {
     try {
-
-        // Get the database and collection on which to run the operation
         const database = client.db("task-manager");
-        const users = database.collection("users");
+        const users = database.collection("tasks");
+        // Create a filter for users with the title "Alejo"
+        const filter = { completed: false };
 
-        const user = await users.find({}).toArray((error, users) => {
-        });
-        
-        const count = await users.countDocuments({});
-        console.log(user);
-        console.log(count);
+        // Specify the update to set a value for the age field
+        const updateDoc = {
+            $set: {
+                completed: true
+            },
+        };
+        // Update the first document that matches the filter
+        const result = await users.updateMany(filter, updateDoc);
 
+        // Print the number of matching and modified documents
+        console.log(
+            `Tasks: ${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+        );
     } finally {
+        // Close the connection after the operation completes
         await client.close();
     }
 }
-run().catch(console.dir);
-
-
-//-----FindOne by Id and Find not completed tasks--------
-async function run() {
-    try {
-        // Get the database and collection on which to run the operation
-        const database = client.db("task-manager");
-        const tasks = database.collection("tasks");
-
-        // Find the last document inserted by sorting based on the _id field in descending order
-        const lastInsertedDocument = await tasks.findOne({}, { sort: { _id: -1 } });
-
-        // Find not completed tasks
-        const notCompleted = await tasks.find({ completed: false }).toArray();
-
-        if (lastInsertedDocument && notCompleted) {
-            console.log("Last Inserted Document:", lastInsertedDocument);
-            console.log("The not completed tasks are:", notCompleted[0].description);
-        } else {
-            console.log("No documents found.");
-        }
-
-    } finally {
-        await client.close();
-    }
-}
-
+// Run the program and print any thrown errors
 run().catch(console.dir);
 
